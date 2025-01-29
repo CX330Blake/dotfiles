d5eab71440f41373846c7092667db5548df33eab
CX330Blake <timchen975@gmail.com>
2025-01-30 06:41:55 +0800

    .

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -8,19 +8,22 @@ return {
                 cmdline_popup = {
                     position = {
                         row = "50%", -- 垂直居中
-                        col = "50%" -- 水平居中
-                    }
+                        col = "50%", -- 水平居中
+                    },
                 },
                 size = {
                     width = 60, -- 設置寬度
-                    height = "auto" -- 高度自適應
+                    height = "auto", -- 高度自適應
                 },
                 border = {
                     style = "rounded", -- 圓角邊框
                     text = {
-                        top = " Command " -- 邊框上方的標題
-                    }
-                }
+                        top = " Command ", -- 邊框上方的標題
+                    },
+                },
+            },
+            presets = {
+                bottom_search = false,
             },
             dependencies = {
                 -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -28,8 +31,8 @@ return {
                 -- OPTIONAL:
                 --   `nvim-notify` is only needed, if you want to use the notification view.
                 --   If not available, we use `mini` as the fallback
-                "rcarriga/nvim-notify"
-            }
-        }
-    }
+                "rcarriga/nvim-notify",
+            },
+        },
+    },
 }
