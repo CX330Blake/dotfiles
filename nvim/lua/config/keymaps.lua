f6b8ae4a68a41a560688639d8a72b55a18881b57
CX330Blake <timchen975@gmail.com>
2025-01-30 00:26:19 -0500

    .

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,23 +7,3 @@ local map = vim.keymap.set
 -- Insert mode keymaps
 
 map("i", "jk", "<Esc>", { noremap = true, silent = true })
-
--- LazyGit
-
-if vim.fn.executable("lazygit") == 1 then
-    map("n", "<leader>gg", function()
-        Snacks.lazygit({ cwd = vim.fn.expand("%:p:h") })
-    end, { desc = "Lazygit (Current File Dir)" })
-    map("n", "<leader>gG", function()
-        Snacks.lazygit()
-    end, { desc = "Lazygit (cwd)" })
-    map("n", "<leader>gf", function()
-        Snacks.picker.git_log_file()
-    end, { desc = "Git Current File History" })
-    map("n", "<leader>gl", function()
-        Snacks.picker.git_log({ cwd = LazyVim.root.git() })
-    end, { desc = "Git Log" })
-    map("n", "<leader>gL", function()
-        Snacks.picker.git_log()
-    end, { desc = "Git Log (cwd)" })
-end
