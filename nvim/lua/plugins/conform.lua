8160bb1eb37bb1b08f23f949fd43bad78190cf2e
CX330Blake <timchen975@gmail.com>
2025-01-30 02:11:17 -0500

    .

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,10 @@
+return {
+    "stevearc/conform.nvim",
+    opts = {
+        formatters = {
+            prettier = {
+                prepend_args = { "--tab-width", "4", "--use-tabs", "false" },
+            },
+        },
+    },
+}
