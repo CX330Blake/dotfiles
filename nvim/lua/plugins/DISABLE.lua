d785c64a7240d4476edc293e368291409a386660
CX330Blake <timchen975@gmail.com>
2025-01-31 09:49:15 -0500

    .

--- nvim/lua/plugins/DISABLE.lua
+++ nvim/lua/plugins/DISABLE.lua
@@ -1,6 +1,6 @@
 return {
     { "simrat39/symbols-outline.nvim", enabled = false },
     { "goolord/alpha-nvim", enabled = false },
-    { "folke/edgy.nvim", enabled = false },
+    { "folke/edgy.nvim", enabled = true },
     { "zbirenbaum/copilot-cmp", enabled = false },
 }
