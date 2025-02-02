92e0185c245503ffa0535ea6553c7ec9b2ec9db5
CX330Blake <timchen975@gmail.com>
2025-02-02 12:39:19 -0500

    .

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -47,30 +47,46 @@ return {
                     },
                     lualine_x = {
                         Snacks.profiler.status(),
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.command.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-            color = function() return { fg = Snacks.util.color("Statement") } end,
-          },
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = function() return { fg = Snacks.util.color("Constant") } end,
-          },
-          -- stylua: ignore
-          {
-            function() return "  " .. require("dap").status() end,
-            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-            color = function() return { fg = Snacks.util.color("Debug") } end,
-          },
-          -- stylua: ignore
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = function() return { fg = Snacks.util.color("Special") } end,
-          },
+                        {
+                            function()
+                                return require("noice").api.status.command.get()
+                            end,
+                            cond = function()
+                                return package.loaded["noice"] and require("noice").api.status.command.has()
+                            end,
+                            color = function()
+                                return { fg = Snacks.util.color("Statement") }
+                            end,
+                        },
+                        {
+                            function()
+                                return require("noice").api.status.mode.get()
+                            end,
+                            cond = function()
+                                return package.loaded["noice"] and require("noice").api.status.mode.has()
+                            end,
+                            color = function()
+                                return { fg = Snacks.util.color("Constant") }
+                            end,
+                        },
+                        {
+                            function()
+                                return "  " .. require("dap").status()
+                            end,
+                            cond = function()
+                                return package.loaded["dap"] and require("dap").status() ~= ""
+                            end,
+                            color = function()
+                                return { fg = Snacks.util.color("Debug") }
+                            end,
+                        },
+                        {
+                            require("lazy.status").updates,
+                            cond = require("lazy.status").has_updates,
+                            color = function()
+                                return { fg = Snacks.util.color("Special") }
+                            end,
+                        },
                         {
                             "diff",
                             symbols = {
@@ -91,7 +107,7 @@ return {
                         },
                     },
                     lualine_y = {
-                        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+                        { "progress", separator = "  ", padding = { left = 1, right = 0 } },
                         { "location", padding = { left = 0, right = 1 } },
                     },
                     lualine_z = {
