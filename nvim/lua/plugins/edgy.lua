9377646614efced2104acd2064a659eb67425a98
CX330Blake <timchen975@gmail.com>
2025-02-09 04:55:20 -0500

    .

--- nvim/lua/plugins/edgy.lua
+++ nvim/lua/plugins/edgy.lua
@@ -17,21 +17,8 @@ return {
             local opts = {
                 animate = {
                     enabled = true,
-                    fps = 100, -- frames per second
-                    cps = 120, -- cells per second
-                    on_begin = function()
-                        vim.g.minianimate_disable = true
-                    end,
-                    on_end = function()
-                        vim.g.minianimate_disable = false
-                    end,
-                    -- Spinner for pinned views that are loading.
-                    -- if you have noice.nvim installed, you can use any spinner from it, like:
-                    -- spinner = require("noice.util.spinners").spinners.circleFull,
-                    spinner = {
-                        frames = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
-                        interval = 80,
-                    },
+                    fps = 100,
+                    cps = 120,
                 },
                 bottom = {
                     {
--- nvim/lua/plugins/smear-cursor.lua
+++ nvim/lua/plugins/smear-cursor.lua
@@ -2,6 +2,24 @@ return {
     -- Smear-cursor
     {
         "sphamba/smear-cursor.nvim",
-        opts = {},
+        event = "VeryLazy",
+        cond = vim.g.neovide == nil,
+        opts = {
+            -- legacy_computing_symbols_support = true,
+            stiffness = 0.6,
+            trailing_stiffness = 0.3,
+            distance_stop_animating = 0.1,
+            -- transparent_bg_fallback_color = "#303030",
+        },
+        specs = {
+            -- disable mini.animate cursor
+            {
+                "echasnovski/mini.animate",
+                optional = true,
+                opts = {
+                    cursor = { enable = false },
+                },
+            },
+        },
     },
 }
