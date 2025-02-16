fc522c138c50e9a423b55528bee528fd7a73c224
CX330Blake <timchen975@gmail.com>
2025-02-16 15:46:37 -0500

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,9 @@ vim.opt.clipboard = "unnamedplus"
 
 -- Cursor line
 vim.opt.cursorline = false
+
+-- Neovide config
+if vim.g.neovide then
+    vim.g.neovide_transparency = 0.75
+    vim.g.transparency = 0.8
+end
