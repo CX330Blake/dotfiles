6ad755c74c3a01be9f014daca0140d79a9284573
CX330Blake <timchen975@gmail.com>
2025-02-23 03:18:01 -0500

    .

--- nvim/lua/plugins/DISABLE.lua
+++ nvim/lua/plugins/DISABLE.lua
@@ -3,4 +3,5 @@ return {
     { "goolord/alpha-nvim", enabled = false },
     { "folke/edgy.nvim", enabled = true },
     { "zbirenbaum/copilot-cmp", enabled = false },
+    { "sphamba/smear-cursor.nvim", enabled = false },
 }
