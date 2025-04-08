dfd75153ac54ee9c071af53f89b94cd7d1afcc26
CX330Blake <timchen975@gmail.com>
2025-04-08 15:56:49 +0800

    Temparary fix paste issue
    It won't show waiting for OSC 52 now

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,9 +13,13 @@ vim.g.clipboard = {
         ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
         ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
     },
+    -- paste = {
+    --     ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    --     ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    -- },
     paste = {
-        ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-        ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+        ["+"] = function() end,
+        ["*"] = function() end,
     },
 }
 
