d21eb7af9f7b6a34a9de3e9b9f662224fb85f592
CX330Blake <timchen975@gmail.com>
2025-05-22 07:03:45 -0400

    always_show_bufferline = false;

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -24,7 +24,7 @@ return {
                 -- stylua: ignore
                 right_mouse_command = function(n) Snacks.bufdelete(n) end,
                 diagnostics = "nvim_lsp",
-                always_show_bufferline = true,
+                always_show_bufferline = false,
                 diagnostics_indicator = function(_, _, diag)
                     local icons = LazyVim.config.icons.diagnostics
                     local ret = (diag.error and icons.Error .. diag.error .. " " or "")
