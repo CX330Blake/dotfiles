d4524ce6dfa1ef5e609dac87590e06e121e49bb6
CX330Blake <timchen975@gmail.com>
2025-09-05 22:46:37 +0800

    Update

--- nvim/lua/plugins/THEME.lua
+++ nvim/lua/plugins/THEME.lua
@@ -72,17 +72,49 @@ return {
             })
         end,
     },
+
+    -- Gruvbox
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = true,
+        opts = {
+            terminal_colors = true, -- add neovim terminal colors
+            undercurl = true,
+            underline = true,
+            bold = true,
+            italic = {
+                strings = false,
+                emphasis = true,
+                comments = true,
+                operators = false,
+                folds = true,
+            },
+            strikethrough = true,
+            invert_selection = false,
+            invert_signs = false,
+            invert_tabline = false,
+            inverse = true, -- invert background for search, diffs, statuslines and errors
+            contrast = "", -- can be "hard", "soft" or empty string
+            palette_overrides = {},
+            overrides = {},
+            dim_inactive = false,
+            transparent_mode = false,
+        },
+    },
+
     -- Configure LazyVim to load dracula
     {
         "LazyVim/LazyVim",
         opts = {
             -- colorscheme = "catppuccin-mocha",
-            colorscheme = "tokyonight-moon",
+            -- colorscheme = "tokyonight-moon",
             -- colorscheme = "tokyonight",
             -- colorscheme = "tokyonight-night"
             -- colorscheme = "tokyonight-storm"
             -- colorscheme = "tokyonight-day",
             -- colorscheme = "tokyonight-moon",
+            colorscheme = "gruvbox",
         },
     },
 }
---
+++ nvim/lua/plugins/themery.lua
@@ -0,0 +1,11 @@
+return {
+    "zaldih/themery.nvim",
+    lazy = false,
+    config = function()
+        require("themery").setup({
+            -- Minimal config
+            themes = { "dracula", "tokyonight-moon", "catppuccin-latte", "catppuccin-mocha", "gruvbox" }, -- Your list of installed colorschemes.
+            livePreview = true, -- Apply theme while picking. Default to true.
+        })
+    end,
+}
