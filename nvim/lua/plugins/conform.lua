743aecce19f9d178d0be27833ac11cf78317b810
CX330Blake <timchen975@gmail.com>
2025-10-31 16:16:22 +0800

    Update python formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,5 +6,11 @@ return {
                 prepend_args = { "--tab-width", "4", "--use-tabs", "false" },
             },
         },
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "isort", "black" },
+            rust = { "rustfmt", lsp_format = "fallback" },
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+        },
     },
 }
